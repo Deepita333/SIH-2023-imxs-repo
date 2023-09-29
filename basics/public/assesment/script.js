6bfd9a1b4227fd2fd92c271e941f2ef4d10419c4
Swarup Saw <bittu28swarup@gmail.com>
2023-09-29 11:39:55 +0530

    do2

--- basics/public/assesment/script.js
+++ basics/public/assesment/script.js
@@ -1,87 +1,121 @@
-const chatLog = document.getElementById("chatLog");
-const userInput = document.getElementById("userInput");
-const sendButton = document.getElementById("sendButton");
-
+let selectedOptions = [];
+let questionIndex = 0;
 const questions = [
-  "Your Mental Health Chatbot",
-  "Please answer the following questions with Yes or No",
-  "Type Ready to Continue",
+  {
+    question: "Over the last two weeks have you felt depressed?",
+    video: "videos/v1.mp4",
+    options: ["Yes", "No"],
+    answer: "Yes",
+  },
+  {
+    question: "Any changes in appetite?",
+    video: "videos/v2.mp4",
+    options: ["Yes", "No"],
+    answer: "Yes",
+  },
+  {
+    question: "Trouble sleeping?",
+    video: "videos/v3.mp4",
+    options: ["Yes", "No"],
+    answer: "Yes",
+  },
+  {
+    question: "Do you feel lack of energy often?",
+    video: "videos/v4.mp4",
+    options: ["Yes", "No"],
+    answer: "Yes",
+  },
+  {
+    question: "Do you feel sad?",
+    video: "videos/v5.mp4",
+    options: ["Yes", "No"],
+    answer: "Yes",
+  },
+];
+
+// Autoplay all the videos
+const videos = document.querySelectorAll('video');
+for (const video of videos) {
+  video.autoplay = true;
+}
+
+function selectOption(option) {
+    // Add the selected option to the array
+    selectedOptions[questionIndex] = option;
   
-  "Do you often feel a sense of hopelessness or worthlessness?",
-  "Do you experience frequent changes in your appetite or weight?",
-  "Do you struggle with constant fatigue or lack of energy?",
-  "Do you have difficulty concentrating or making decisions?",
-  "Do you frequently experience feelings of sadness or tearfulness?",
-  "Do you find it challenging to enjoy activities or hobbies that used to bring you pleasure?",
-  "Do you often feel anxious, restless, or on edge?",
-  "Do you have trouble falling asleep, staying asleep, or experiencing restful sleep?",
-  "Do you have recurrent thoughts of death or suicidal ideation?",
-  "Do you feel a sense of isolation or disconnection from others?"
-];
-
-let currentQuestion = 0;
-let noCount = 0;
-
-function generateChatBubble(text, isUser) {
-  const chatBubble = document.createElement("div");
-  chatBubble.classList.add("chat-bubble");
-  chatBubble.classList.add(isUser ? "user" : "bot");
-  chatBubble.innerText = text;
-  chatLog.appendChild(chatBubble);
-  chatLog.scrollTop = chatLog.scrollHeight;
-}
-
-function askQuestion() {
-  generateChatBubble(questions[currentQuestion], false);
-  currentQuestion++;
-}
-
-function processUserInput() {
-  const answer = userInput.value;
-  generateChatBubble(answer, true);
-
-  if (answer.toLowerCase() === "yes") {
-    noCount++;
-  }
-
-  if (currentQuestion < questions.length) {
-    askQuestion();
-  } else {
-    if (noCount > 5) {
-      alert("It is recommended that you consult a therapist.");
-    } else {
-      generateChatBubble("Thank you for sharing. It appears that you are mentally fine! Remember to cherish each moment, find joy in the little things, and nurture your mental well-being. You deserve to lead a fulfilling and happy life. Stay positive and embrace the beauty that surrounds you. Wishing you continued happiness and contentment on your journey!", false);
+    // Clear previous selection
+    if (selectedOptions[questionIndex - 1]) {
+      document.querySelector(`.${selectedOptions[questionIndex - 1]}`).classList.remove('selected');
     }
-    userInput.disabled = true;
-    sendButton.disabled = true;
+  
+    // Set the selected option and apply the click effect
+    const selectedButton = document.querySelector(`.${option}`);
+    selectedButton.classList.add('selected');
+  
+    // Apply the click effect (scale down)
+    selectedButton.style.transform = 'scale(0.95)';
+  
+    // Reset the scale after a short delay
+    setTimeout(() => {
+      selectedButton.style.transform = 'scale(1)';
+    }, 100);
   }
+  
 
-  userInput.value = "";
+function submitAnswers() {
+  if (selectedOptions[questionIndex]) {
+    // Update the question index
+    questionIndex++;
+
+    // Check if there are any more questions
+    if (questionIndex < questions.length) {
+      // Display the next question
+      const nextQuestion = questions[questionIndex];
+      document.querySelector('.question h2').textContent = nextQuestion.question;
+      document.querySelector('.question video').src = nextQuestion.video;
+      document.querySelector('.options').innerHTML = '';
+
+      for (const option of nextQuestion.options) {
+        const optionDiv = document.createElement('div');
+        optionDiv.classList.add('option');
+        optionDiv.textContent = option;
+        optionDiv.onclick = () => selectOption(option);
+        document.querySelector('.options').appendChild(optionDiv);
+      }
+    } else {
+      // Calculate the score
+      const yesCount = selectedOptions.filter((option) => option === 'Yes').length;
+
+      if (yesCount > 3) {
+        alert('You may be experiencing depression. Please reach out to a mental health professional for help.');
+      } else {
+        alert('You seem to be doing fine, but it is always a good idea to check in with yourself and your mental health.');
+      }
+
+      // Hide the submit button
+      document.querySelector('#submit').style.display = 'none';
+
+      // Hide the circle buttons
+      document.querySelector('.options').style.display = 'none';
+    }
+  } else {
+    alert('Please select an option before submitting.');
+  }
 }
 
-askQuestion();
-sendButton.addEventListener("click", processUserInput);
-userInput.addEventListener("keyup", function(event) {
-  if (event.key === "Enter") {
-    processUserInput();
-  }
-});
-// Sample doctor data (replace with your actual data)
-const doctorsData = [
-  { name: "Dr. Smith", specialization: "Psychologist", fees: "$100" },
-  { name: "Dr. Johnson", specialization: "Therapist", fees: "$80" },
-  // Add more doctors as needed
-];
+// Display the first question
+const firstQuestion = questions[0];
+document.querySelector('.question h2').textContent = firstQuestion.question;
+document.querySelector('.question video').src = firstQuestion.video;
+document.querySelector('.options').innerHTML = '';
 
-// Function to populate doctor listings
-function populateDoctorList() {
-  const doctorSelect = document.getElementById("doctor");
-
-  doctorsData.forEach((doctor, index) => {
-      const option = document.createElement("option");
-      option.value = index;
-      option.textContent = `${doctor.name} - ${doctor.specialization} (${doctor.fees})`;
-      doctorSelect.appendChild(option);
-  });
+for (const option of firstQuestion.options) {
+  const optionDiv = document.createElement('div');
+  optionDiv.classList.add('option');
+  optionDiv.textContent = option;
+  optionDiv.onclick = () => selectOption(option);
+  document.querySelector('.options').appendChild(optionDiv);
 }
 
+// Show the submit button
+document.querySelector('#submit').style.display = 'block';
